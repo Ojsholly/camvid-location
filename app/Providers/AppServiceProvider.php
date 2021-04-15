bd502a62aff278e4f194d64a1a47bd4b4b1931a2
Olusola Ojewunmi <olusolaojewunmi@gmail.com>
2021-04-15 15:34:12 +0100

    Add SSL redirection and schema length.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if (env('APP_ENV') == 'production') {
+            URL::forceScheme('https');
+        }
+
+        Schema::defaultStringLength(191);
     }
 }
